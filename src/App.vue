aaf5a4762176f2e92ec20a58bd56da6319b8dda2
Ali-Chouchene <choucheneali9801@gmail.com>
2023-01-26 14:12:00 +0100

    start & added bootstrap

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script >
+
+</script>
+
+<template>
+  <header class="bg-dark">
+    <div class="container d-flex justify-content-between p-4">
+      <div><img class="w-50" src="./assets/img/boolflix.png" alt="boolflix"></div>
+      <div class="d-flex align-items-center">
+        <form class="d-flex" role="search">
+          <input class="form-control me-1" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-danger" type="button">Search</button>
+        </form>
+      </div>
+
+    </div>
+  </header>
+</template>
+
+<style >
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+/**BOOTSTAP */
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const app = createApp(App)
+
+
+/**registro componenti */
+app.mount('#app')
