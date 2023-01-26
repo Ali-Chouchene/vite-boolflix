2cc0b0b277287698883d78302f272a9718a338fa
Ali-Chouchene <choucheneali9801@gmail.com>
2023-01-26 14:40:08 +0100

    store created

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,59 @@
 <script >
-
+import axios from 'axios'
+import { store } from '../src/data/store'
+export default {
+  data() {
+    return {
+      store,
+    }
+  },
+  methods: {
+    fetchMovies(url) {
+      axios.get(url)
+        .then(res => {
+          store.movies = res.data.results;
+        })
+    },
+  }
+}
 </script>
 
 <template>
+  <!--header-->
   <header class="bg-dark">
     <div class="container d-flex justify-content-between p-4">
       <div><img class="w-50" src="./assets/img/boolflix.png" alt="boolflix"></div>
       <div class="d-flex align-items-center">
         <form class="d-flex" role="search">
-          <input class="form-control me-1" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-danger" type="button">Search</button>
+          <input class="form-control me-1" type="search" placeholder="Cerca" aria-label="Search">
+          <button class="btn btn-outline-danger" type="button">Trova</button>
         </form>
       </div>
-
     </div>
   </header>
+  <!--main-->
+  <main class="bg-secondary">
+    <div class="container py-5">
+      <h1 class="py-5">FILM</h1>
+      <ul>
+        <li></li>
+      </ul>
+    </div>
+  </main>
 </template>
 
-<style >
+<style scoped lang="scss" >
+/* appheader*/
+header {
+  position: sticky;
+  top: 0;
+  left: 0;
+  right: 0;
+}
 
+
+/* appmain */
+// main {
+//   background-color: gray;
+// }
 </style>
---
+++ src/data/store.js
@@ -0,0 +1,16 @@
+import { reactive } from 'vue';
+export const store = reactive({
+
+    /** original api + movie OR tv(series) */
+    apiUri: 'https://api.themoviedb.org/3/search/',
+
+    /**api key */
+    apiKey: '?api_key=b481e93ec7c1abb9a9cd959e916cea8b',
+    /**query + SEARCHED WORD */
+    apiTerm: '&query=',
+
+    /**movies array */
+    movies: [],
+    /**series array */
+    tv: [],
+})
