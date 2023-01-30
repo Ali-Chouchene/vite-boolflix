9c0a2a6cd7b8e2c4d38be6bb2c57fe46e6b54272
Ali-Chouchene <choucheneali9801@gmail.com>
2023-01-30 17:40:56 +0100

    added navigation button

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -3,9 +3,10 @@ import CardSection from './subcomponents/CardSection.vue'
 import { store } from '../../src/data/store'
 import WelcomePage from './subcomponents/WelcomePage.vue'
 import Loader from './subcomponents/Loader.vue'
+import NavButton from './subcomponents/NavButton.vue'
 export default {
     name: 'AppMain',
-    components: { CardSection, WelcomePage, Loader },
+    components: { CardSection, WelcomePage, Loader, NavButton },
     data() {
         return {
             store
@@ -17,10 +18,11 @@ export default {
 </script>
 <template>
     <main class=" py-2">
+        <nav-button></nav-button>
         <welcome-page v-if="!store.movies.length && !store.isLoading && !store.series.length"></welcome-page>
         <loader v-if="store.isLoading"></loader>
-        <card-section collection='movies' title="FILM"></card-section>
-        <card-section collection='series' title="SERIE TV"></card-section>
+        <card-section id="movies" collection='movies' title="FILM"></card-section>
+        <card-section id="series" collection='series' title="SERIE TV"></card-section>
     </main>
 </template>
 
---
+++ src/components/subcomponents/NavButton.vue
@@ -0,0 +1,42 @@
+<script>
+import { store } from '../../data/store'
+export default {
+    name: 'NavButton',
+    components: {},
+    data() {
+        return {
+            store
+        }
+    },
+}
+
+</script>
+
+<template>
+    <div class="btn-group ms-5">
+        <button type="button" class="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+            Choice
+        </button>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#movies">Movies</a></li>
+            <li><a class="dropdown-item" href="#series">Series</a></li>
+        </ul>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.btn-group {
+    position: fixed;
+    top: 22vh;
+    left: 150px;
+
+
+}
+
+.dropdown-toggle {
+    width: 70px;
+    height: 70px;
+    border-radius: 50%;
+    background-color: red;
+}
+</style>
